9c05441db6831a8f150a51558999d34dbb3f3e90
chloejung <66489400+chloejung@users.noreply.github.com>
2020-08-06 16:07:19 -0700

    Hides 'prepare' table on mobile, displays collapsible

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Overlay
+// Overlay for mobile menu
 
 $(document).ready(function() {
 		$(".nav-icon").click(function(){ 
@@ -15,3 +15,21 @@ $(document).ready(function() {
 		$(".nav-icon a").toggleClass('btn-open').toggleClass('btn-close');
 		open = false; // tracking variable to see if its on or off (hiding overlay)
 	});
+
+
+// Collapsible 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
